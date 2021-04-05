e6ebdfa76504a5b69a697391267ea0669511f605
victorzele96 <57448124+victorzele96@users.noreply.github.com>
2021-04-05 17:20:05 +0300

    update branch

---
+++ StudyBuddy/forms.py
@@ -0,0 +1,39 @@
+from django.forms import ModelForm,TextInput
+from .models import *
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+
+class TeacherUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(TeacherUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.is_staff = True
+            user.save()
+        return user
+
+
+
+class StudentUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(StudentUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.is_staff = False
+            user.save()
+        return user
