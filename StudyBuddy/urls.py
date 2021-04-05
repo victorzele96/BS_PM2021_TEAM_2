29257f1aa37e4481ac6899fda2dfe3ed16966828
EugeneSCE <eugengo@ac.sce.ac.il>
2021-04-05 17:11:49 +0300

    BSPM2021T2-27 register,login,logout link to code and frontend

--- StudyBuddy/urls.py
+++ StudyBuddy/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
+
+from django.conf.urls import url
+from django.urls import path,include
+from django.views.generic import ListView,DetailView
+
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    # path('', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+
+    path("register_teacher/", views.register_teacher, name="register_teacher"),
+    path("register_student/", views.register_student, name="register_student"),
+    path("login/", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout"),
 ]
