b58f91b1c527dc8b41fe0cfe9c2f12fb7e5c44c7
EugeneSCE <eugengo@ac.sce.ac.il>
2021-04-10 18:39:55 +0300

    BSPM2021T2-27 news model

--- StudyBuddy/models.py
+++ StudyBuddy/models.py
@@ -2,3 +2,17 @@ from django.db import models
 from django.contrib.auth.forms import UserChangeForm
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField()
+    body = models.TextField()
+    date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        #for admin
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
