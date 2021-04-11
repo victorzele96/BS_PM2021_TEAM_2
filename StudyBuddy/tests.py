11eaece310de6138f4f5786f3ec39c7203d2bdae
Arie <57365299+jayzpkz@users.noreply.github.com>
2021-04-11 19:52:08 +0300

    tests.py => change setup and teardown methods to cass methods

--- StudyBuddy/tests.py
+++ StudyBuddy/tests.py
@@ -5,43 +5,47 @@ from django.test import TestCase
 # Login tests
 class LoginTest(TestCase):
 
-    def setUp(self):
-        print("\n___Login SetUp___")
-        self.user = get_user_model().objects.create_user(username='admin', password='admin')
-        self.user.save()
+    @classmethod
+    def setUpClass(cls):
+        super(LoginTest, cls).setUpClass()
+        print("___Login SetUp___")
+        cls.user = get_user_model().objects.create_user(username='admin', password='admin')
+        cls.user.save()
 
-    def tearDown(self):
-        print("___Login TearDown___\n")
-        self.user.delete()
+    @classmethod
+    def tearDownClass(cls):
+        super(LoginTest, cls).tearDownClass()
+        print("\n___Login TearDown___")
+        cls.user.delete()
 
     def test_correct(self):
         user = authenticate(username='admin', password='admin')
         test = user is not None and user.is_authenticated
         self.assertTrue(test)
-        print("Correct Login - ", test)
+        print("\nCorrect Login - ", test)
 
     def test_wrong_username(self):
         user = authenticate(username='wrong', password='admin')
         test = user is not None and user.is_authenticated
         self.assertFalse(test)
-        print("Wrong Username Login - ", test)
+        print("\nWrong Username Login - ", test)
 
     def test_wrong_password(self):
         user = authenticate(username='admin', password='wrong')
         test = user is not None and user.is_authenticated
         self.assertFalse(test)
-        print("Wrong Password Login - ", test)
+        print("\nWrong Password Login - ", test)
 
     def test_wrong_input(self):
         user = authenticate(username='wrong', password='wrong')
         test = user is not None and user.is_authenticated
         self.assertFalse(test)
-        print("Wrong Input Login - ", test)
+        print("\nWrong Input Login - ", test)
 # Login tests
 
-# Register tests
-
 
 # Register tests
+class RegisterTest(TestCase):
 
-
+    pass
+# Register tests
