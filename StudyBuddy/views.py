d9c7636a3b592cde2765383cc63768519d7de647
EugeneSCE <eugengo@ac.sce.ac.il>
2021-05-28 15:58:02 +0300

    update after arie

--- StudyBuddy/views.py
+++ StudyBuddy/views.py
@@ -424,7 +424,7 @@ def View_Sched(request, pk):
 #                   template_name="../templates/school/MY_TEST.html",
 #                   context={"form": form})
 
-
+git
 def create_class(request):
 
     if request.method == "POST":
