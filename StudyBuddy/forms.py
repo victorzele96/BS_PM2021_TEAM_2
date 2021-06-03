8d5b85f95ceeda51304a8110c8dbc5ec6de0c76f
elenach2 <elenach.work@gmail.com>
2021-06-03 17:07:44 +0300

    BSPM2021T2-227

--- StudyBuddy/forms.py
+++ StudyBuddy/forms.py
@@ -79,9 +79,20 @@ class TeacherUserForm(UserCreationForm):
 
 class ClassroomForm(forms.ModelForm):
 
+    classroom = Classroom.objects.all()
+    list_of_ids = []
+    for c in classroom:
+        list_of_ids.append(c.teacher.id)
+    user = User.objects.filter(id__in=list_of_ids)
+
+    list_id = []
+    for temp_obj in (User.objects.filter(is_staff=True) & User.objects.filter(is_superuser=False)):
+        if temp_obj not in user:
+            list_id.append(temp_obj.id)
+    qs = User.objects.filter(id__in=list_id)
+
     teacher = forms.ModelChoiceField(
-        # queryset=User.objects.get(is_staff=True, is_superuser=False),
-        queryset=User.objects.filter(is_staff=True) & User.objects.filter(is_superuser=False),
+        queryset=qs,
         initial=0
     )
 
@@ -96,7 +107,6 @@ class ClassroomForm(forms.ModelForm):
         if commit:
             classroom.save()
         return classroom
-
 
 
 class StudentClassroomForm(forms.ModelForm):
